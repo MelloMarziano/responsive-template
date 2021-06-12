0050130e23f3bf01a80dcd0c56c298158f398765
Eliu Ortega <eliu.orteg@gmail.com>
2021-06-12 19:57:20 -0400

    deleted unnecessary files and added the main component of the layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { LayoutComponent } from './layout/layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'Home',
+    pathMatch: 'full'
+
+  },
+  {
+    path: 'Home',
+    component: LayoutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
